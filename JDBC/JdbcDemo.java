1467fb0129cf60f024ea3f9240b8b05f50612423
AynodkarVC <aynodkar@hotmail.com>
2024-04-10 17:27:48 +0530

    Jdbc demo

--- JDBC/JdbcDemo.java
+++ JDBC/JdbcDemo.java
@@ -8,15 +8,19 @@ public class JdbcDemo {
         String url = "jdbc:mysql://localhost:3306/JdbcDemo";
         String name = "root";
         String pass = "Asphalt8@";
-        String query = "select username from student where userId = 3";
+        String query = "select * from students";
 
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(url,name,pass);
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(query);
-        rs.next();
-        String userName = rs.getString("username");
-        System.out.println(userName);
+        String userData =" ";
+        for (int i = 0; i <= 2; i++){
+            rs.next();
+            userData = rs.getInt(1)+" : "+rs.getString(2);
+            System.out.println(userData);
+       }
+
         st.close();
         con.close();
     }
